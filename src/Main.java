cd4bc40d8e92cccea780c8ab11dfbd8ed16f89c7
lurdakas <lkasileviciute@protonmail.com>
2024-05-22 15:55:11 +0300

    Sixth, Seventh, Eighth tasks finished.

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,13 @@ public class Main {
         System.out.println("___Penkta___");
         Skaicius(10, 20);
         System.out.println(Skaicius(10, 20));
-
+        System.out.println("___Sesta___");
+        int[] RndmSk = RandomSkaiciai(2, 5, 5);
+        System.out.println(RndmSk);
+        System.out.println("___Septinta___");
+        System.out.println("Suma " + MasyvoSuma(RandomSkaiciai(2, 5, 5)));
+        System.out.println("___Astunta___");
+        System.out.println("Vidurkis " + MasyvoVidurkis(RandomSkaiciai(2, 5, 5)));
 
 
     }
@@ -42,19 +48,78 @@ public class Main {
             System.out.println(num[i]);
         }
 
-        }
+
+    }
 
 ///   Penkta uzduotis.
 
 
-
     public static int Skaicius(int min, int max) {
-        return (int) (max + (Math.random() * (max - min) - min));
+        return (int) (Math.random() * (max - min) + min);
 
-
+    }
 
 
 ///   Sesta uzduotis.
+
+
+    public static int[] RandomSkaiciai(int min1, int max1, int length) {
+        int[] RndmSk = new int[length];
+        for (int i = 0; i < length; i++) {
+            RndmSk[i] = (int) (Math.random() * (max1 - min1) + min1);
+            System.out.println(RndmSk[i]);
+        }
+        return RndmSk;
+    }
+
+///   Septinta uzduotis.
+
+
+    public static int MasyvoSuma(int[] RndmSk) {
+        int sum = 0;
+        for (int i = 0; i < RndmSk.length; i++) {
+            sum += RndmSk[i];
+        }
+        return sum;
+    }
+
+
+///   Astunta uzduotis.
+
+
+    public static double MasyvoVidurkis(int[] arr) {
+        double average = 0;
+        for (int i = 0; i < arr.length; i++) {
+            average += arr[i];
+        }
+        return average / arr.length;
+    }
+
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
